7fa18c3ff50040b011011fc2f9be8d93517aeced
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-08-16 19:38:34 +0300

    reposition middleware and adjust sample application data

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ const PORT = 3000;
 //absolute path for the SQLiteDB file
 const dbPath = path.resolve(__dirname, '../job_tracker.db');
 
+app.use(express.json());
+
 //opening connection to the database
 const db = new sqlite3.Database(dbPath, (err) => {
   if(err) {
@@ -33,9 +35,9 @@ db.get("SELECT COUNT(*) as count FROM applications", (err, row) => {
   }
   if (row.count === 0) {
     const insert = 'INSERT INTO applications (company, position, status) VALUES (?, ?, ?)';
-    db.run(insert, ['OpenAI', 'Backend Developer', 'Applied']);
-    db.run(insert, ['Google', 'Frontend Developer', 'Interview']);
-    db.run(insert, ['Microsoft', 'Full Stack Developer', 'Rejected']);
+    db.run(insert, ['Company1', 'Backend Developer', 'Applied']);
+    db.run(insert, ['Company2', 'Frontend Developer', 'Interview']);
+    db.run(insert, ['Company3', 'Full Stack Developer', 'Rejected']);
     console.log("Test data inserted into applications table.");
   }
 });
@@ -55,7 +57,7 @@ app.get('/applications', (req, res) => {
 
 app.post('/applications', (req, res) => {
   const { company, position, date_applied, status, notes } = req.body;
-  app.use(express.json());
+  
 
   const insert = 'INSERT INTO applications (company, position, date_applied, status, notes) VALUES (?, ?, ?, ?, ?)';
   
@@ -66,8 +68,7 @@ app.post('/applications', (req, res) => {
     }
     res.json({ id: this.lastID, company, position, date_applied, status, notes });
   });
-
-})
+});
 
 //starting the server and listening on port 3001
 app.listen(PORT, () => {
